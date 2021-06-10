737d7ddad356ebe41692865245ede19097546ee5
a_sindhu <asindhubhargavi@gmail.com>
2021-06-10 15:25:26 +0530

    DB Connection Established

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dbConnect = require('./backend/lib/connectLib');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+dbConnect.connect();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
